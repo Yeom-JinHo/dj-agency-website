17d2e9a4da824d589908d07051780fbdae67e53b
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2026-01-13 22:31:11 +0900

    chore: react-scan & Web Analytics 활성화 (#15)
    * chore: react-scan 활성화
    
    * refactor: payday-records return type
    
    * chore: Web Analytics
    
    * feat: github auto labeling
    
    * feat: labeling workflow 추가
    
    * fix: workflow 파일 경로 수정

--- apps/vague-frequency-labs/src/app/layout.tsx
+++ apps/vague-frequency-labs/src/app/layout.tsx
@@ -3,12 +3,11 @@ import { Bricolage_Grotesque } from "next/font/google";
 
 import "@/styles/globals.css";
 
-import Script from "next/script";
 import { metadata as meta } from "@/app/config";
 import Providers from "@/app/providers";
 import { ErrorBoundary } from "@repo/ui/common/ErrorBoundary";
 import { createMetadata } from "@/utils";
-// import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next";
 // import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Footer, Header } from "./sections";
 
@@ -36,7 +35,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {process.env.NODE_ENV === "development" && (
-          <Script
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
             crossOrigin="anonymous"
             src="https://unpkg.com/react-scan/dist/auto.global.js"
           />
@@ -52,7 +52,7 @@ export default function RootLayout({
                 <Footer />
               </footer>
             </div>
-            {/* <Analytics /> */}
+            <Analytics />
             {/* <SpeedInsights /> */}
             {/* <Cursor /> */}
           </Providers>
